5d59c475eebfbdcbc0cab2768cbd082bdf83a089
safacaki <safacaki@yahoo.com>
2020-12-04 20:16:02 +0300

    saga installed

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import { createBrowserHistory } from "history";
 import { routerMiddleware } from "connected-react-router";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers";
 
 export const history = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer(history),
-  composeEnhancer(applyMiddleware(routerMiddleware(history)))
+  composeEnhancer(applyMiddleware(routerMiddleware(history), sagaMiddleware))
 );
 export default store;
